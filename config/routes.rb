daa4a5a0e8c076ff958cd6b18c7bc8842f591c6c
Andreas Cichocki <github@icaptain.de>
2021-12-12 09:58:16 +0100

    route for edit users data

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users, :skip => [:registrations]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_for :users, :skip => [:registrations]
+  as :user do
+    get "/sign_in" => "devise/sessions#new" # custom path to login/sign_in
+    # get "/sign_up" => "devise/registrations#new", as: "new_user_registration" # custom path to sign_up/registration
+    get 'users/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'
+    put 'users' => 'devise/registrations#update', :as => 'user_registration'
+  end
 
   # Defines the root path route ("/")
   root "home#index"
