827e303b747fb46f363d5407efcc2c5f165deb90
Andreas Cichocki <github@icaptain.de>
2021-12-12 10:23:03 +0100

    better devise routing to root
    instead of get "/" with a 401 and the redirecting to users/sign_in we are now using root to sessions#new in the devise_scope :user

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   devise_for :users, :skip => [:registrations]
-  as :user do
-    get "/sign_in" => "devise/sessions#new" # custom path to login/sign_in
-    # get "/sign_up" => "devise/registrations#new", as: "new_user_registration" # custom path to sign_up/registration
-    get 'users/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'
-    put 'users' => 'devise/registrations#update', :as => 'user_registration'
-  end
 
   # Defines the root path route ("/")
-  root "home#index"
+  authenticated :user do
+    root "home#index", as: :authenticated_root
+  end
+
+  devise_scope :user do
+    # get "/sign_up" => "devise/registrations#new", as: "new_user_registration" # custom path to sign_up/registration
+    get "users/edit" => "devise/registrations#edit", :as => "edit_user_registration"
+    put "users" => "devise/registrations#update", :as => "user_registration"
+    root to: "devise/sessions#new"
+  end
 end
